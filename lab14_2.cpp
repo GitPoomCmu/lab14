22c964300fbaebcee027bedfd29fce15d373b87a
GitPoomCmu <152594220+GitPoomCmu@users.noreply.github.com>
2024-01-05 17:42:07 +0700

    Update lab14_2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,36 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool input[][N]){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            cout << input[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void inputMatrix(double input[][N]){
+    for(int i=0; i<N; i++){
+        cout << "Row " << i+1 << ": ";
+        cin >> input[i][0] >> input[i][1] >> input[i][2] >> input[i][3] >> input[i][4];
+    }
+}
+
+void findLocalMax(const double mA[][N], bool mB[][N]){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            double b = mA[i][j];
+            if(i==0 || i==N-1 || j==0 || j==N-1){
+                mB[i][j] = false;
+            }
+            else if(mA[i+1][j] > b || mA[i-1][j] > b || mA[i][j+1] > b || mA[i][j-1] > b){
+                mB[i][j] = false;
+            }
+            else{
+                mB[i][j] = true;   
+            }
+        }
+    }
+}
